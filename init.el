b46bf3ff974f8c64f3dd2d4fc894844e3e30fd95
Chetan Yewale <yewalechetan@gmail.com>
2021-07-11 11:51:59 +0530

    updated init.el

--- init.el
+++ init.el
@@ -1,10 +1,17 @@
-;; numbers in emacs everywhere
-
+;;; package --- Summary
+;;; Commentary:
+;; Chetan's Emacs config
 ;; Added by Package.el.  This must come before configurations of
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
+(require 'package)
+(setq package-archives '(("org"       . "http://orgmode.org/elpa/")
+                         ("gnu"       . "http://elpa.gnu.org/packages/")
+                         ("melpa"     . "http://melpa.org/packages/")))
 (package-initialize)
+;;(package-refresh-contents) ;;this contacts all repos and slows down.. enable only if you want to refresh packages
+
 
 (global-linum-mode t)
 
@@ -18,31 +25,23 @@
 (set-face-attribute 'default nil :height 160)
 
 ;; set cursor type
-(setq cursor-type '(bar . 1))
+(setq-default cursor-type 'bar)
 
 ;;hide toolbar
 (tool-bar-mode -1)
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
 
-;;word wrap settings text mode
-(add-hook 'text-mode-hook '(lambda ()
-    (setq truncate-lines nil
-          word-wrap t
-	  visual-line-mode t)))
+(save-place-mode 1)
 
-;;word wrap settings prog mode
-(add-hook 'prog-mode-hook '(lambda ()
-    (setq truncate-lines t
-          word-wrap nil
-	  visual-line-mode t)))
+;;word wrap settings
+(setq visual-line-mode t)
 
-
-
+;;; Prevent Extraneous Tabs
+(setq-default indent-tabs-mode nil)
 ;; symbolic links
 (setq vc-follow-symlinks t)
 
-(require 'package)
-
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -50,7 +49,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (grandshell-theme clues-theme cherry-blossom-theme distinguished-theme color-theme-sanityinc-tomorrow alect-themes cyberpunk-theme tron-legacy-theme treemacs-icons-dired treemacs-all-the-icons afternoon-theme abyss-theme treemacs-projectile projectile material-theme flycheck-pycheckers flycheck flycheck-cfn cfn-mode magit treemacs go-mode zenburn-theme))))
+    (yafolding json-mode grandshell-theme cherry-blossom-theme distinguished-theme alect-themes cyberpunk-theme tron-legacy-theme treemacs-icons-dired treemacs-all-the-icons afternoon-theme treemacs-projectile projectile project material-theme flycheck-pycheckers flycheck flycheck-cfn cfn-mode magit treemacs go-mode zenburn-theme))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -109,3 +108,30 @@
 ;;if frame is closed, then automatically save the session
 (add-to-list 'delete-frame-functions #'desktop-save)
 
+;; keys for indenting and unindenting
+(defun my-indent-region (N)
+  (interactive "p")
+  (if (use-region-p)
+      (progn (indent-rigidly (region-beginning) (region-end) (* N 1))
+             (setq deactivate-mark nil))
+    (self-insert-command N)))
+
+(defun my-unindent-region (N)
+  (interactive "p")
+  (if (use-region-p)
+      (progn (indent-rigidly (region-beginning) (region-end) (* N -1))
+             (setq deactivate-mark nil))
+    (self-insert-command N)))
+
+(global-set-key ">" 'my-indent-region)
+(global-set-key "<" 'my-unindent-region)
+
+
+;;; copied from better defaults
+(global-set-key (kbd "C-s") 'isearch-forward-regexp)
+(global-set-key (kbd "C-r") 'isearch-backward-regexp)
+(global-set-key (kbd "C-M-s") 'isearch-forward)
+(global-set-key (kbd "C-M-r") 'isearch-backward)
+
+(provide 'init)
+;;; init.el ends here
